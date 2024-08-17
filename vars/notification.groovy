dacd58e75ce869222cd2ff642353339281ab80a6
Manshu Sharma <36654754+Svastikkka@users.noreply.github.com>
2024-08-18 00:00:26 +0530

    update: webhook url

--- vars/notification.groovy
+++ vars/notification.groovy
@@ -1,5 +1,5 @@
 def call(message) {
-    def googleChatWebhook = "https://chat.googleapis.com/v1/spaces/AAAAKYaF-4w/messages?key=AIzaSyDdI0hCZtE6vySjMm-WEfRq3CPzqKqqsHI&token=iB_dMvNwRu4NvGPBwGbf8lyTBXI_z7cm4gNCJLs2BNE"
+    def googleChatWebhook = "WEBHOOK_URL"
     def buildUrl = env.BUILD_URL
     def completeMessage = "Pipeline job ${env.JOB_NAME} - ${message} and build URL: http://192.168.0.209:8080/job/${env.JOB_NAME}/${env.BUILD_NUMBER}"
 
